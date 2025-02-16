d05074af27a9d3ad73d3685bce40b4f78e9120ea
Vedant <vedant10vaidya@gmail.com>
2025-02-16 18:34:20 +0900

    Spring MVC | handling complex forms | Error handling & display on top of form using BindingResult

--- src/main/java/springmvc/search/controller/FormController.java
+++ src/main/java/springmvc/search/controller/FormController.java
@@ -1,6 +1,7 @@
 package springmvc.search.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +17,11 @@ public class FormController {
 	}
 	
 	@RequestMapping(path="/handleform", method = RequestMethod.POST)
-	public String handleForm(@ModelAttribute("student") Student student) {
+	public String handleForm(@ModelAttribute("student") Student student, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "complex_form";
+		}
 		
 		System.out.println(student);
 		System.out.println(student.getAddress());
--- src/main/webapp/WEB-INF/views/complex_form.jsp
+++ src/main/webapp/WEB-INF/views/complex_form.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!doctype html>
 <html lang="en">
 	<head>
@@ -25,6 +25,10 @@
 				<div class="card">
 					<div class="card-body">
 						<h3 class="text-center">Complex Form</h3>
+						
+						<div class="alert alert-danger" role="alert">
+  							<form:errors path="student.*" />
+						</div>
 
 						<form action="handleform" method="post">
 
