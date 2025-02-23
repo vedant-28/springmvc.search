006f010c13a5fc7eee6cde112bb8899c7ecddaa3
Vedant <vedant10vaidya@gmail.com>
2025-02-24 02:39:35 +0900

    Spring MVC | File uploading | completed

--- src/main/java/springmvc/search/controller/FileUploadController.java
+++ src/main/java/springmvc/search/controller/FileUploadController.java
@@ -1,12 +1,19 @@
 package springmvc.search.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.springframework.http.MediaType;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class FileUploadController {
@@ -21,12 +28,42 @@ public class FileUploadController {
 					method = RequestMethod.POST, 
 					consumes = "multipart/form-data"
 					)
-	public String fileupload(@RequestParam("profile") MultipartFile file) {
+	public String fileupload(@RequestParam("profile") MultipartFile file,
+							 HttpSession session,
+							 Model model) throws IOException {
 		
 		System.out.println(file.getSize());
 		System.out.println(file.getContentType());
 		System.out.println(file.getName());
 		System.out.println(file.getOriginalFilename());
+		
+		byte[] data = file.getBytes();
+		String path = session.getServletContext().getRealPath("/") 
+							 + "WEB-INF" 
+							 + File.separator 
+							 + "resources"
+							 + File.separator
+							 + "images"
+							 + File.separator
+							 + file.getOriginalFilename();
+		System.out.println(path);
+		
+		try {
+			// Storing uploaded file in server
+			FileOutputStream fos = new FileOutputStream(path);
+			fos.write(data);
+			fos.close();
+			System.out.println("File uploaded...");
+			
+			// Sending uploaded file name to view for showing on success page
+			model.addAttribute("filename", file.getOriginalFilename());
+			model.addAttribute("message", "Uploaded successfully...");
+			
+		} catch (IOException e) {
+			System.out.println("Uploading error...");
+			model.addAttribute("message", "Something went wrong...");
+			e.printStackTrace();
+		}
 		
 		return "filesuccess";
 	}
--- src/main/webapp/WEB-INF/views/filesuccess.jsp
+++ src/main/webapp/WEB-INF/views/filesuccess.jsp
@@ -1,12 +1,18 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" 
+    	 contentType="text/html; charset=UTF-8"
+    	 pageEncoding="UTF-8"
+    	 isELIgnored="false"
+%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Upload sucess!</title>
+		<title>Upload result</title>
 	</head>
 	<body>
-		<h1>File uploaded successfully...</h1>
+		<h1>${message }</h1>
+		<img alt="uploaded image" src="<c:url value="WEB-INF/resources/image/${filename }" />" />
 	</body>
 </html>
