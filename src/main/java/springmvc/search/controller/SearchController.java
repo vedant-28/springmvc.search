82a561bbb2d65446a01fbad325a3f519aea54fe0
Vedant <vedant10vaidya@gmail.com>
2025-02-24 14:58:33 +0900

    Spring MVC | @PathVariable | Completed

--- src/main/java/springmvc/search/controller/SearchController.java
+++ src/main/java/springmvc/search/controller/SearchController.java
@@ -1,12 +1,20 @@
 package springmvc.search.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SearchController {
+	
+	@RequestMapping("/user/{userId}")
+	public String getUserDetails(@PathVariable("userId") int userId) {
+		System.out.println(userId);		
+		return "home";
+	}
+	
 	
 	@RequestMapping("/home")
 	public String home() {
